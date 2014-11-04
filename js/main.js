99f0c255a7d39cf6933622f914061649f4692f43
SteffiB <schirkapinto@Schirkas-iMac.local>
2014-11-04 11:25:25 -0500

    Start/End screens
    All start and end screens. Used an extra index file (index2.com)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$("#killPets").click(function() {
+	$(".frame.start-screen").css("display", "none");
+	$(".frame.kill-pets").css("display", "block");
+});
+$("#killPetsNext").click(function() {
+	$(".frame.kill-pets").css("display", "none");
+	$(".frame.kill-commissioner").css("display", "block");
+});
+$("#startkilling").click(function() {
+	$(".frame.kill-commissioner").css("display", "none");
+	$(".frame.play-active").css("display", "block");
+});
 
+$("#savePets").click(function() {
+	$(".frame.start-screen").css("display", "none");
+	$(".frame.save-pets").css("display", "block");
+});
+$("#savePetsNext").click(function() {
+	$(".frame.save-pets").css("display", "none");
+	$(".frame.save-commissioner").css("display", "block");
+});
+$("#startsaving").click(function() {
+	$(".frame.save-commissioner").css("display", "none");
+	$(".frame.play-active").css("display", "block");
+});
