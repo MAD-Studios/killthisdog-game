2f1d568d37b1714adb3547271986e2091cfcab5d
SteffiB <schirkapinto@Schirkas-iMac.local>
2014-11-10 14:45:57 -0500

    animations

--- js/main.js
+++ js/main.js
@@ -30,3 +30,76 @@ $( document ).ready(function() {
 		sound.playSound("beer_can_opening");
 	})
 });
+
+function hit(hitArea, type, state){
+	if($(hitArea + " .on")){
+		$(".hit-targets" + hitArea).addClass(type);
+		setTimeout(function() {
+    		$(".hit-targets" + hitArea).removeClass(type);
+    		$(".hit-targets" + hitArea).addClass(state);
+    		document.getElementById('pointCount').innerHTML= parseInt(document.getElementById('pointCount').innerHTML)+parseInt($(hitArea + " .on")[0].getAttribute("data-scoreVal"));
+    		setTimeout(function() {
+    			$(".hit-targets" + hitArea).removeClass(state);
+    			$(".hit-targets" + hitArea + " div").removeClass("on");
+    			$(".hit-targets" + hitArea + " div").addClass("off");
+    			createNewPet(".hit-targets" + hitArea);
+			}, 1000);
+		}, 1000);
+	}
+}
+
+function createNewPet(hitArea){
+	random = Math.ceil(Math.random() * 3);
+	console.log(random);
+	switch(random) {
+    case 1:
+        $(hitArea + " div.dog-type-l").removeClass("off");
+    	$(hitArea + " div.dog-type-l").addClass("on");
+        break;
+    case 2:
+        $(hitArea + " div.dog-type-s").removeClass("off");
+    	$(hitArea + " div.dog-type-s").addClass("on");
+        break;
+    case 3:
+        $(hitArea + " div.cat-type-m").removeClass("off");
+    	$(hitArea + " div.cat-type-m").addClass("on");
+        break;
+    default:
+        break;
+	}
+}
+
+$(".hit-targets.location-dumpster").click(function() {
+	hit(".location-dumpster", "hit", "kill");
+});
+$(".hit-targets.location-trashcan1").click(function() {
+	hit(".location-trashcan1", "hit", "kill");
+});
+$(".hit-targets.location-trashcan2").click(function() {
+	hit(".location-trashcan2", "hit", "kill");
+});
+$(".hit-targets.location-bag").click(function() {
+	hit(".location-bag", "hit", "kill");
+});
+$(".hit-targets.location-manhole").click(function() {
+	hit(".location-manhole", "hit", "kill");
+});
+$(".hit-targets.location-cardboardBox").click(function() {
+	hit(".location-cardboardBox", "hit", "kill");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
